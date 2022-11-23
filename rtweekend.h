8abe4883ff197362aac942248e09f3a65b47cc93
Chi Zhang <aaronzhang9797@outlook.com>
2022-11-23 14:19:09 -0500

    random number generator

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 
@@ -17,6 +18,13 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
 
 inline double degrees_to_radians(double degrees)
 {
